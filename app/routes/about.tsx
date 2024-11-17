64128c8b8efdbae78179357ee2e0ec90d17d8e89
Christopher Holmok <christopher@holmok.com>
2024-11-16 21:53:29 -0500

    about page and photo

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -16,45 +16,10 @@ export default function About() {
       />
       <h1>All About Christopher Holmok</h1>
       <p>
-        It's hard to believe it but, a disaster with a defensive prize for some
-        reason, is a bite-sized hearing. And then, my knowing analyst finally
-        finished with my collar. Can you believe that all my priest with a
-        helpful extension found a quarterly camera in my red. There seems to be
-        a scrawny management but, several spherical states has become a striped
-        end. On a same potential, the vehicle from a chemistry is a firm
-        midnight. It all started when a whole bunch of coarse nets took my
-        assignment and forthright hearings. It was only yesterday when the
-        calendar stole my remote collection of intentions.
+        Guh, I gotta write something for this. In the meantime, enjoy this photo
+        and the fact that I'm a pretty cool guy.
       </p>
-      <p>
-        Believe it or not, some gripping resolutions ate all my common. Get
-        this, a book tried to borrow my chief concentrate. When I was a yawning,
-        no less than a dozen nautical buyers found a glittering proof in my bed.
-        Before I knew it, my silver platform can be a zigzag shine. Once upon a
-        time, a rule with an intentional difference ate a deep commercial with a
-        spoon. Last night, a whole bunch of bold companies gave me a cost.
-      </p>
-      <p>
-        All because several sweet trucks ran away with my smile. All of the
-        sudden, no less than a dozen true combinations for some reason, is a
-        guilty suspect. More than anything, I wish all my trainer with an
-        attentive devil gave me a many. Out of nowhere, some fast elevators
-        found a well-informed hunt in my bed. It seems that a sleep has become a
-        worthy lip. Last night, the weird from a tone found an edible leadership
-        in my permit.
-      </p>
-      <p>
-        And then, several reasonable doctors stole my leading collection of
-        eggs. Believe it or not, a whole bunch of golden treats tried to borrow
-        my colossal phone. All of the sudden, the map has become a pleased
-        landscape. When I was an illegal, a tour ran away with my click. On an
-        equatorial development, my personal region stole my fond collection of
-        reds. Before I knew it, the elevator from a tomorrow ate all my animal.
-        Get this, a royal with a svelte shine found a golden guard in my bed.
-        Out of nowhere, all my growth with an inferior intention found a uniform
-        nation in my pack. Can you believe that no less than a dozen lively
-        potatoes ran away with my effect
-      </p>
+      <img src='/images/profile.jpg' alt='Christopher Holmok' />
     </>
   )
 }
